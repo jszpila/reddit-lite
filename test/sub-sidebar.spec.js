79a890eb9d5556a82316ab153847760ec48791a6
Jan Szpila <jszpila@gmail.com>
2017-06-02 00:07:15 -0500

    Implements initial tests

--- test/sub-sidebar.spec.js
+++ test/sub-sidebar.spec.js
@@ -1,11 +1,15 @@
-var assert  = require('assert'),
-    Vue     = require('vue'),
-    sidebar = '../src/js/components/sub-sidebar';
+import 'jsdom-global/register';
+import assert     from 'assert';
+import Vue        from 'vue';
+import subSideBar from '../src/js/components/sub-sidebar.vue';
 
 describe('sub-sidebar.vue', function() {
-  describe('#decode', function() {
+  describe('#decodeHTML', function() {
     it('should convert escaped text to HTML', function() {
-      assert(true);
+      const Constructor = Vue.extend(subSideBar);
+      const comp = new Constructor().$mount();
+
+      assert(comp.decodeHTML('&lt;div class=\"test-class\"&gt;&lt;p&gt; hello this is a test &lt;/p&gt;&lt;/div&gt;'), '<div class="test-class"><p> hello this is a test </p></div>');
     });
   });
 });
--- test/utc-date-time.spec.js
+++ test/utc-date-time.spec.js
@@ -1,11 +1,14 @@
-var assert      = require('assert'),
-    Vue         = require('vue'),
-    utcDateTime = '../src/js/components/utc-date-time';
+import assert      from 'assert';
+import Vue         from 'vue';
+import utcDateTime from '../src/js/components/utc-date-time.vue';
 
 describe('utc-date-time.vue', function() {
   describe('#formatDate', function() {
     it('convert a UTC time stap to M/D/YYYY h:mma format', function() {
-      assert(true);
+      const Constructor = Vue.extend(utcDateTime);
+      const comp = new Constructor().$mount();
+
+      assert(comp.formatDate('1496376816'), '6/1/2017, 11:13pm');
     });
   });
 });
