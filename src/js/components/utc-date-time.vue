31fc645e08212354671282fb5cdd4e6b57d0d404
Jan Szpila <jszpila@gmail.com>
2017-06-02 13:21:50 -0500

    Corrects issue with minute display

--- src/js/components/utc-date-time.vue
+++ src/js/components/utc-date-time.vue
@@ -6,10 +6,12 @@ export default Vue.component('rl-utc-date-time', {
       formatDate: function(utcStr) {
         var d        = new Date(0),
             h        = null,
+            m        = null,
             meridian = 'am';
 
         d.setUTCSeconds(utcStr);
         h = d.getHours();
+        m = d.getMinutes();
 
         if (h > 12) {
           h -= 12;
@@ -18,7 +20,11 @@ export default Vue.component('rl-utc-date-time', {
           h = 12;
         }
 
-        return ((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ', ' + h + ':' + d.getMinutes() + meridian);
+        if (m < 10) {
+          m = "0" + m.toString();
+        }
+
+        return ((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ', ' + h + ':' + m + meridian);
       }
     },
     template: `<span v-bind:class="cssClass">{{txt}} {{formatDate(utc)}}</span>`
