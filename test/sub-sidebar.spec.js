b74ce9339dedc556e809902d15fcd127bc4447be
Jan Szpila <jszpila@gmail.com>
2017-06-07 13:00:16 -0500

    Adds another test for sidebar component

--- test/sub-sidebar.spec.js
+++ test/sub-sidebar.spec.js
@@ -12,4 +12,13 @@ describe('sub-sidebar.vue', function() {
       assert(comp.decodeHTML('&lt;div class=\"test-class\"&gt;&lt;p&gt; hello this is a test &lt;/p&gt;&lt;/div&gt;'), '<div class="test-class"><p> hello this is a test </p></div>');
     });
   });
+
+  describe('#decodeHTML', function() {
+    it('should NOT alter text that does not contain encoded characters', function() {
+      const Constructor = Vue.extend(subSideBar);
+      const comp = new Constructor().$mount();
+
+      assert(comp.decodeHTML('tHe qu!ck brown $% fox jumP3d ()ver the lAzy d0ge!'), 'tHe qu!ck brown $% fox jumP3d ()ver the lAzy d0ge!');
+    });
+  });
 });
