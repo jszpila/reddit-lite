f27ca1ef75d1009a39b493c8a2a46780f0ff495d
Jan Szpila <jszpila@gmail.com>
2017-06-07 12:36:45 -0500

    Adds additional text for bad timestamp data

--- src/js/components/utc-date-time.vue
+++ src/js/components/utc-date-time.vue
@@ -3,28 +3,38 @@ import Vue from 'vue';
 export default Vue.component('rl-utc-date-time', {
     props: ['utc', 'txt', 'cssClass'],
     methods: {
+      // Convert a UTC time stamp to a human-readable date in M/D/YYY h:mma format
+      // @utcStr - timestamp - UTC time stamp
       formatDate: function(utcStr) {
-        var d        = new Date(0),
-            h        = null,
-            m        = null,
-            meridian = 'am';
+        var formattedDate = '';
 
-        d.setUTCSeconds(utcStr);
-        h = d.getHours();
-        m = d.getMinutes();
+        try {
+          var d        = new Date(0),
+              h        = null,
+              m        = null,
+              meridian = 'am';
 
-        if (h > 12) {
-          h -= 12;
-          meridian = 'pm';
-        } else if (h === 0) {
-          h = 12;
+          d.setUTCSeconds(utcStr);
+          h = d.getHours();
+          m = d.getMinutes();
+
+          if (h > 12) {
+            h -= 12;
+            meridian = 'pm';
+          } else if (h === 0) {
+            h = 12;
+          }
+
+          if (m < 10) {
+            m = "0" + m.toString();
+          }
+
+          formattedDate =  ((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ', ' + h + ':' + m + meridian);
+        } catch (e) {
+          formattedDate = 'invalid date';
+        } finally {
+          return formattedDate;
         }
-
-        if (m < 10) {
-          m = "0" + m.toString();
-        }
-
-        return ((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear() + ', ' + h + ':' + m + meridian);
       }
     },
     template: `<span v-bind:class="cssClass">{{txt}} {{formatDate(utc)}}</span>`
--- test/utc-date-time.spec.js
+++ test/utc-date-time.spec.js
@@ -4,11 +4,21 @@ import utcDateTime from '../src/js/components/utc-date-time.vue';
 
 describe('utc-date-time.vue', function() {
   describe('#formatDate', function() {
-    it('convert a UTC time stamp to M/D/YYYY h:mma format', function() {
+    it('should convert a UTC time stamp to M/D/YYYY h:mma format', function() {
       const Constructor = Vue.extend(utcDateTime);
       const comp = new Constructor().$mount();
 
       assert(comp.formatDate('1496376816'), '6/1/2017, 11:13pm');
     });
   });
+
+  describe('#formatDate', function() {
+    it('should output "invalid date" when provided with bad data', function() {
+      const Constructor = Vue.extend(utcDateTime);
+      const comp = new Constructor().$mount();
+
+      assert(comp.formatDate('50me G4rb!j txT'), 'invalid date');
+    });
+  });
+
 });
