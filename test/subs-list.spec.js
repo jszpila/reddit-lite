0f58be600180843fd5034561d052c925dca462f5
Jan Szpila <jszpila@gmail.com>
2017-06-08 11:53:31 -0500

    Adds test for subs-list watched props

--- test/subs-list.spec.js
+++ test/subs-list.spec.js
@@ -2,6 +2,27 @@ import assert   from 'assert';
 import subsList from '../src/js/components/subs-list.vue';
 
 describe('subs-list.vue', function() {
+  describe('#watch - term', function() {
+    it('should reset subs when term is changed', function(done) {
+      let Constructor = Vue.extend(subsList);
+      let comp = new Constructor({
+        propsData: {
+          domain:   'https://www.reddit.com/',
+          term:     'default',
+          activeSub: null
+        }
+      }).$mount();
+
+      comp.subs = [{dispay_name_prefixed: 'r/test'}, {dispay_name_prefixed: 'r/lolwat'}];
+      comp.term = 'changed!';
+
+      Vue.nextTick(() => {
+        assert(comp.subs === null);
+        done();
+      });
+    });
+  });
+
   describe('#getSubReddits - popular', function() {
     it('should populate 25 default subreddits', function(done) {
       let Constructor = Vue.extend(subsList);
