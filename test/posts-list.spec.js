09222d893b70b5ab154ca5fc9acde5b0c7c796ee
Jan Szpila <jszpila@gmail.com>
2017-06-08 12:42:19 -0500

    Implements watch and created tests for subs-list

--- test/posts-list.spec.js
+++ test/posts-list.spec.js
@@ -2,6 +2,86 @@ import assert    from 'assert';
 import postsList from '../src/js/components/posts-list.vue';
 
 describe('posts-list.vue', function() {
+  describe('#created - with activeSub', function() {
+    it('should populate 25 posts from r/AskReddit', function(done) {
+      let Constructor = Vue.extend(postsList);
+      let comp = new Constructor({
+        propsData: {
+          domain:   'https://www.reddit.com/',
+          term:     '',
+          activeSub: {
+            display_name_prefixed: 'r/AskReddit'
+          }
+        }
+      }).$mount();
+
+      comp.getPosts(function() {
+        assert((comp.posts.length === 25));
+        return done();
+      });
+    });
+  });
+
+  describe('#created - without activeSub', function() {
+    it('should NOT populate any posts', function(done) {
+      let Constructor = Vue.extend(postsList);
+      let comp = new Constructor({
+        propsData: {
+          domain:    'https://www.reddit.com/',
+          term:      '',
+          activeSub: null
+        }
+      }).$mount();
+
+      Vue.nextTick(() => {
+        assert(comp.posts === null);
+        done();
+      });
+    });
+  });
+
+  describe('#watch - activeSub', function() {
+    it('should populate 25 posts from r/eli5/ AND updated title when the activeSub is changed', function(done) {
+      let Constructor = Vue.extend(postsList);
+      let comp = new Constructor({
+        propsData: {
+          domain:   'https://www.reddit.com/',
+          term:     '',
+          activeSub: {
+            display_name_prefixed: 'r/AskReddit'
+          }
+        }
+      }).$mount();
+
+      comp.activeSub = {
+        display_name_prefixed: 'r/eli5'
+      };
+
+      Vue.nextTick(() => {
+        assert(comp.activeSub.display_name_prefixed === 'r/eli5');
+        done();
+      });
+    });
+  });
+
+  describe('#watch - searching', function() {
+    it('should NOT populate any posts', function(done) {
+      let Constructor = Vue.extend(postsList);
+      let comp = new Constructor({
+        propsData: {
+          domain:    'https://www.reddit.com/',
+          term:      '',
+          activeSub: null
+        }
+      }).$mount();
+
+      Vue.nextTick(() => {
+        assert(comp.posts === null);
+        done();
+      });
+    });
+  });
+
   describe('#getPosts', function() {
     it('should populate 25 posts from r/AskReddit', function(done) {
       let Constructor = Vue.extend(postsList);
