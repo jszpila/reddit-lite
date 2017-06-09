bb0a4cf0389aa380124431ec21a79b2a90aa7a43
Jan Szpila <jszpila@gmail.com>
2017-06-09 10:00:47 -0500

    Cleaning up tests

--- test/header.spec.js
+++ test/header.spec.js
@@ -2,38 +2,6 @@ import assert from 'assert';
 import header from '../src/js/components/header.vue';
 
 describe('header.vue', function() {
-  describe('#created - without term', function() {
-    it('submit button should be initially disabled', function() {
-      const Constructor = Vue.extend(header);
-      const comp = new Constructor({
-        propsData: {
-          searching: false
-        }
-      }).$mount();
-
-      // ???
-
-      assert(true);
-    });
-  });
-
-  describe('#created - with term', function() {
-    it('submit button should be enabled when there is a search term', function() {
-      const Constructor = Vue.extend(header);
-      const comp = new Constructor({
-        propsData: {
-          searching: false
-        }
-      }).$mount();
-
-      this.term = 'test';
-
-      // ???
-
-      assert(true);
-    });
-  });
-
   describe('#watch - term', function() {
     it('busy should false when searching is true', function(done) {
       const Constructor = Vue.extend(header);
